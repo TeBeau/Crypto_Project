678afe26608afb54b4d9380bf3c6fa058a83d4a2
pinkoa2 <pinkoa2.rpi.edu>
2020-08-09 14:03:24 -0400

    Added Bank File

--- client.py
+++ client.py
@@ -34,13 +34,14 @@ def Diffie_Hellman_Key_Exchange():
 s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 s.connect( (socket.gethostname(), 1234) )
 
+# Assume this is Completely Hidden
 users= dict()
-users['Lauren']= 101
-users['Alex']= 97
-users['JJ']= 79
+users['Lauren'] = 1013
+users['Alex'] = 9742
+users['JJ'] = 7959
+# Assume this is Completely Hidden
 
 right_pin=False
-
 while right_pin == False:
 	username= input("Input your username:")
 	pin= int(input("Input your pin#:"))
